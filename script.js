c6b3ee891bcb9916546fc29a066f67cd460974ed
betit13 <beyetito377@gmail.com>
2024-11-25 13:33:48 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+"use strict";
+// Manipulate the 3 button elements
+// Manipulate the Popup and Overlay
+// Keypress Event using the addEventListener
+
+// The first thing to do during manipulation is to select the Elemment
+
+const btnOpenModal = document.querySelectorAll(".btn");
+const btnCloseModal = document.querySelector(".close-btn");
+const OverlayEl = document.querySelector(".overlay");
+const popupEl = document.querySelector(".popup");
